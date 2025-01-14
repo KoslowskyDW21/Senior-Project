87a209cfb9d88e93a5d676462171329b0270b227
KoslowskyDW21 <KoslowskyDW21@gcc.edu>
2025-01-14 11:55:57 -0500

    config.py cleanup

--- config.py
+++ config.py
@@ -5,14 +5,11 @@ from dotenv import load_dotenv
 basedir: str = os.path.abspath(os.path.dirname(__file__))
 # load any config options from the environment variable file
 load_dotenv(os.path.join(basedir, '.appenv'))
-scriptdir = os.path.dirname(os.path.abspath(__file__))
-dbfile = os.path.join(scriptdir, "users.sqlite3")
 
 # define a config singleton to be used in configuring apps
 class Config:
-    # for each config option either find it in an environment variable or use a default
+    # for each config option find it in an environment variable
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # NOTE this is using a temporary local database, use the commented out environmental
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # during debugging disable caching (remove for final version)
