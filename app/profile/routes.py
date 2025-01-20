1e5370e3c92f3bff1db1bb5feeef619a6f878448
Andrew Marston <marston@Andrews-MacBook-Air.local>
2025-01-19 20:31:25 -0500

    Connected frontend and backend for profile

--- app/profile/routes.py
+++ app/profile/routes.py
@@ -1,12 +1,20 @@
 from __future__ import annotations
-from flask import render_template
+from flask import jsonify
 from app.profile import bp
 from app.models import User, db
 
-@bp.get("/<int:id>/")
-def get_profile_page(id):
+@bp.route('/<int:id>', methods=['POST'])
+def post_profile_page(id):
     print("searching for user " + str(id))
     user = User.query.filter_by(id=id).first()
     if user is not None:
-        return render_template('profile.html', user=user)
+        return jsonify({"id": str(user.id)}), 200 # TODO: refactor to return more than just an id
     return "<h1>404: profile not found</h1>", 404
+
+@bp.route('/', methods=['POST'])
+def post_default_profile_page():
+    print("Getting default user profile")
+    user = User.query.filter_by(id=1).first()
+    if user is not None:
+        return jsonify({"id": "1"}), 200
+    return "<h1>Something is wrong in the database: user 1 does not exist</h1>"
--- senior-project-react/src/components/Profile.tsx
+++ senior-project-react/src/components/Profile.tsx
@@ -1,9 +1,29 @@
+import axios from "axios";
 import React from "react";
 
+interface ProfileResponse {
+    id: string; // could have some problems here
+}
+
 const Profile: React.FC = () => {
+    const [id, setId] = React.useState<String>();
+
+    const getResponse = async () => {
+        const response = await axios.post(
+            "http://127.0.0.1:5000/profile/2" //TODO: refactor to allow the changing of the user id
+        );
+        const data: ProfileResponse = response.data;
+        setId(data.id);
+    }
+
+React.useEffect(() => {
+    getResponse();
+}, []);
+
     return (
         <>
             <h1>This is a profile page!!</h1> {/* TODO: replace with actual account information */}
+            <h2>This profile belongs to the user with id={id}</h2>
         </>
     )
 }
