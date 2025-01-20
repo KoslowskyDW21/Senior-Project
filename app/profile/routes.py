06098ab3b883cf2d52faa19713e146076d565753
Andrew Marston <marston@Andrews-MacBook-Air.local>
2025-01-20 13:54:57 -0500

    Improved profile page

--- app/profile/routes.py
+++ app/profile/routes.py
@@ -1,20 +1,18 @@
 from __future__ import annotations
-from flask import jsonify
+from flask import jsonify, redirect, url_for
 from app.profile import bp
 from app.models import User, db
 
 @bp.route('/<int:id>', methods=['POST'])
-def post_profile_page(id):
+def post_profile_page(id=1):
     print("searching for user " + str(id))
     user = User.query.filter_by(id=id).first()
     if user is not None:
-        return jsonify({"id": str(user.id)}), 200 # TODO: refactor to return more than just an id
+        return jsonify({ "lname": user.lname,
+                         "fname": user.fname
+                         }), 200
     return "<h1>404: profile not found</h1>", 404
 
 @bp.route('/', methods=['POST'])
 def post_default_profile_page():
-    print("Getting default user profile")
-    user = User.query.filter_by(id=1).first()
-    if user is not None:
-        return jsonify({"id": "1"}), 200
-    return "<h1>Something is wrong in the database: user 1 does not exist</h1>"
+    return redirect(url_for('post_profile_page'))
--- senior-project-react/src/components/Profile.tsx
+++ senior-project-react/src/components/Profile.tsx
@@ -1,29 +1,54 @@
 import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
 import React from "react";
+import { Button, TextField, Container } from "@mui/material"; //matui components
 
 interface ProfileResponse {
-    id: string; // could have some problems here
+    lname: string;
+    fname: string;
 }
 
 const Profile: React.FC = () => {
-    const [id, setId] = React.useState<String>();
+
+    const navigate = useNavigate(); //for navigation
+
+    let { id } = useParams<{ id: string }>();
+    if (id == undefined) {
+        id = "1";
+    }
+    const [lname, setLname] = React.useState<String>();
+    const [fname, setFname] = React.useState<String>();
 
     const getResponse = async () => {
         const response = await axios.post(
-            "http://127.0.0.1:5000/profile/2" //TODO: refactor to allow the changing of the user id
+            `http://127.0.0.1:5000/profile/${id}`
         );
         const data: ProfileResponse = response.data;
-        setId(data.id);
+        setLname(data.lname);
+        setFname(data.fname);
     }
 
 React.useEffect(() => {
     getResponse();
 }, []);
 
+const handleGoToRecipes = async () => {
+    console.log("Navigating to recipes page");
+    navigate(`/recipes`);
+  }
+
     return (
         <>
-            <h1>This is a profile page!!</h1> {/* TODO: replace with actual account information */}
-            <h2>This profile belongs to the user with id={id}</h2>
+            <h1>This is a profile page!!</h1> {/* TODO: replace with fuller account information */}
+            <h2>This is {fname} {lname}'s profile!</h2>
+
+            <Button
+                onClick={handleGoToRecipes}
+                variant="contained"
+                color="primary"
+            >
+                Recipes
+            </Button>
         </>
     )
 }
