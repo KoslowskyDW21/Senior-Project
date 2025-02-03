a03706e91b19a3ef5a5c1cb157e175a86ceb2e57
Jeffrey Krug <krugjl21@gcc.edu>
2025-02-02 21:01:08 -0500

    Got the admin page working

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -8,5 +8,13 @@ from flask_login import current_user, login_required
 @bp.route("/", methods=["GET"])
 def is_admin():
     user = current_user._get_current_object()
+
+    print(user)
+
     return jsonify(user.to_json()), 200 # type: ignore
-    return jsonify({"isAdmin": user.is_admin}), 200 # type: ignore
+
+@login_required
+@bp.route("/users/", methods=["GET"])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_json() for user in users])
--- senior-project-react/src/components/AdminPage.tsx
+++ senior-project-react/src/components/AdminPage.tsx
@@ -4,26 +4,79 @@ import React from "react";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-async function isAdmin(admin: boolean) {
-  await axios.get("http://127.0.0.1:5000/admin/") // TODO: Add the URL
-    .then((response) => {
-      admin = response.data.is_admin;
-    })
-    .catch((error) => {
-      console.log("Could not retrieve data: ", (error));
-    })
-
-  admin = false;
+interface User {
+  id: number;
+  fname: string;
+  lname: string;
+  email_address: string;
+  username: string;
+  profile_picture: string;
+  xp_points: number;
+  user_level: number;
+  is_admin: boolean;
+  num_recipes_completed: number;
+  colonial_floor: string;
+  colonial_side: string;
+  date_create: Date;
+  last_logged_in: Date;
+  num_reports: number;
 }
 
 export default function AdminPage() {
-  let admin: boolean = false;
-  React.useEffect(() => {isAdmin(admin);}, [])
-
+  const [admin, setAdmin] = useState<boolean>(false);
+  const [users, setUsers] = useState<User[]>([]);
   const navigate = useNavigate();
 
+  async function isAdmin() {
+    await axios.get("http://127.0.0.1:5000/admin/")
+      .then((response) => {
+        setAdmin(response.data.is_admin);
+      })
+      .catch((error) => {
+        console.error("Unable to check if user is admin", error)
+      })
+  }
+
+  async function loadUsers() {
+    await axios.get("http://127.0.0.1:5000/admin/users/")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Could not load users", error);
+      })
+  }
+  
+  React.useEffect(() => {isAdmin(); loadUsers();}, []);
+
   if(admin) {
-    return <h1>Admin Page</h1>;
+    return (
+      <>
+        <h1>Admin Page</h1>
+        <br />
+        <h2>Users</h2>
+        <table>
+          <thead>
+            <tr>
+              <td>Email</td>
+              <td>Username</td>
+              <td>Admin Status</td>
+              <td>Reports</td>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.email_address}</td>
+                <td>{user.username}</td>
+                <td>{user.is_admin.toString()}</td>
+                <td>{user.num_reports}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    )
   }
   else {
     return (
