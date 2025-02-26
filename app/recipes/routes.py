4f83d7e3bc8ba7ad7f483bdfd90ba89bde8b4b6d
KoslowskyDW21 <KoslowskyDW21@gcc.edu>
2025-02-26 18:24:23 -0500

    Re-fixed search after breaking it

--- app/recipes/routes.py
+++ app/recipes/routes.py
@@ -133,7 +133,10 @@ def post_recipes():
     total_pages = ceil(recipes_paginated.total / per_page)  # type: ignore
 
     # Convert the results to JSON using `to_json()`
-    recipes = [recipe[0].to_json() for recipe in recipes_paginated.items]
+    if not search_query:
+        recipes = [recipe[0].to_json() for recipe in recipes_paginated.items]
+    else:
+        recipes = [recipe.to_json() for recipe in recipes_paginated.items]
 
     return jsonify({
         'recipes': recipes,
