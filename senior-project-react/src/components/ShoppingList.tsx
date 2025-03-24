6e442522b1ff839d726385c24e403a71044e5c43
ahorriblegoose <bibighauske21@gcc.edu>
2025-03-24 15:36:50 -0400

    little fixes

--- senior-project-react/src/components/ShoppingList.tsx
+++ senior-project-react/src/components/ShoppingList.tsx
@@ -309,7 +309,7 @@ const ShoppingList: React.FC = () => {
   return (
     <>
       {/* Back button */}
-      <IconButton onClick={() => navigate("/recipes/")} style={{ position: "fixed", top: 30, left: 30 }}>
+      <IconButton onClick={() => navigate("/recipes")} style={{ position: "fixed", top: 30, left: 30 }}>
         <ArrowBackIcon sx={{ fontSize: 30, fontWeight: "bold" }} />
       </IconButton>
 
